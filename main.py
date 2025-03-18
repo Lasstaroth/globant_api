9a9000fc1c4f52d31a7c4cff0cc46f5be860d2bf
Lasstaroth <jose.villanueva.salgado@gmail.com>
2025-03-17 23:33:52 -0500

    Configured FastAPI app with database initialization and routing, including a health check endpoint

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from typing import Union
-
 from fastapi import FastAPI
+import models
+from database import engine
+from routes import router
 
-app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
+app = FastAPI(title="Globant API")
+
+app.include_router(router)
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def root():
+    return {"message": "API is running"}
