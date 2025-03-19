47e4d2eb236c209a7a4f07eb72f84713ef59bd88
Lasstaroth <jose.villanueva.salgado@gmail.com>
2025-03-18 19:03:16 -0500

    Enable sqlite as a database for testing

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://admin:admin@localhost:5433/globant_db"
+# DATABASE_URL = "postgresql://postgres:admin@localhost:5433/globant_db"
+DATABASE_URL = "sqlite:///./globant_db.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
